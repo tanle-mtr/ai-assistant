d03e4bc2c4e17fb2c4a5ed985759c220995c7c34
tanle <tan120528@126.com>
2026-08-19 10:56:31 +0800

    Add Android build infrastructure for GitHub Actions
    - Add gradlew wrapper scripts
    - Complete Android project structure
    - Update GitHub Actions workflow to use system Gradle
    - Add drawable resources for app icons

---
+++ android/gradlew.bat
@@ -0,0 +1,7 @@
+﻿@rem Gradle startup script for Windows
+@if "%DEBUG%"=="" @echo off
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+set APP_HOME=%DIRNAME%
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+"%JAVA_HOME%\bin\java.exe" -Xmx1536m -Xms1536m -Dfile.encoding=UTF-8 -Duser.country=CN -Duser.language=zh -cp "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
